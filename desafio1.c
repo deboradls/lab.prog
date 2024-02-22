82a2ef99d4222f48de21fb9d4209f3b329e9b57e
Débora <deb.limad@gmail.com>
2024-02-22 10:30:16 -0300

    Desafio 1

---
+++ desafio1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1, num2;
+
+  printf("Digite o primeiro número: ");
+  scanf("%d", &num1);
+
+  printf("Digite o segundo número: ");
+  scanf("%d", &num2);
+
+  int diff = num1 - num2;
+  int sign = (diff >> 31) & 0x1; // Obtém o bit de sinal do número diferença
+
+  // Se o bit de sinal for 0, num1 é maior, senão num2 é maior
+  int maior = num1 - sign * diff;
+
+  printf("O número maior é: %d\n", maior);
+
+  return 0;
+}
