f1db0ad2fe113b665fa6e12fabe20a11dfee5d72
Débora <deb.limad@gmail.com>
2024-03-11 16:39:10 -0300

    Questão 1

--- Atividade 2/q01.c
+++ Atividade 2/q01.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int fibonacci(int n, int memo[]) {
-    if (n <= 1) {
+int fibonacci(int n, int memo[])
+{
+    if (n <= 1)
+    {
         return n;
     }
 
-    if (memo[n] != -1) {
+    if (memo[n] != -1)
+    {
         return memo[n];
     }
 
@@ -14,23 +17,27 @@ int fibonacci(int n, int memo[]) {
     return memo[n];
 }
 
-int main() {
+int main()
+{
     int n;
 
     // Solicita ao usuário para inserir o valor de n dentro do intervalo permitido
-    do {
+    do
+    {
         printf("Digite um inteiro entre 0 e 30 para n: "); // solicitei que fosse entre 0 e 30 pois números grandes o programa demora a fazer o cálculo
         scanf("%d", &n);
 
         // Verifica se n está dentro do intervalo permitido
-        if (n < 0 || n > 30) {
+        if (n < 0 || n > 30)
+        {
             printf("Por favor, insira um inteiro entre 0 e 30.\n");
         }
     } while (n < 0 || n > 30);
 
     // Inicializa o array de memoização com -1 (indicando que nenhum valor foi calculado ainda)
     int memo[n + 1];
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
+    {
         memo[i] = -1;
     }
 
