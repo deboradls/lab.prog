4f0b61fd1f6c7c684794785f79d4ec5543f4138c
Débora <deb.limad@gmail.com>
2024-03-25 10:49:49 -0300

    Questão 3

--- Atividade 4/q03.c
+++ Atividade 4/q03.c
@@ -2,89 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-#include <string.h>
 
-
-typedef struct Senha {
-    char codigo[7]; 
-    bool isPrioridade; 
-    struct Senha *prox; 
-} Senha;
-
-void gerarSenha(char senha[]) {
-    if (rand() % 3 == 0) {
-       
-        int i;
-        for (i = 0; i < 2; i++) {
-            senha[i] = 'P'; 
-        }
-        for (i = 2; i < 5; i++) {
-            senha[i] = 'A' + rand() % 26; 
-        }
-        senha[5] = rand() % 10 + '0'; 
-        senha[6] = '\0'; 
-    } else {
-      
-        int i;
-        for (i = 0; i < 3; i++) {
-            senha[i] = 'A' + rand() % 26;
-        }
-        senha[3] = rand() % 10 + '0'; 
-        senha[4] = '\0'; 
+void gerarSenha(int *senha, bool *isPrioridade)
+{
+    if (rand() % 3 == 0)
+    {
+        *isPrioridade = true;
+        *senha = rand() % 1000;
+    }
+    else
+    {
+        *isPrioridade = false;
+        *senha = rand() % 100;
     }
 }
 
-void mostrarTodasSenhas(Senha *inicio) {
-    Senha *atual = inicio;
+void mostrarTodasSenhas(int *senhas, bool *prioridades, int numSenhasGeradas)
+{
     printf("Todas as senhas geradas:\n");
-    while (atual != NULL) {
-        printf("Senha: %s (%s)\n", atual->codigo, atual->isPrioridade ? "Prioridade" : "Normal");
-        atual = atual->prox;
+    for (int i = 0; i < numSenhasGeradas; i++)
+    {
+        printf("Senha: %03d (%s)\n", senhas[i], prioridades[i] ? "Prioridade" : "Normal");
     }
 }
 
-void consumirSenhaAleatoria(Senha **inicio) {
-    if (*inicio != NULL) {
-        int totalSenhas = 0;
-        Senha *atual = *inicio;
-        while (atual != NULL) {
-            totalSenhas++;
-            atual = atual->prox;
+void consumirSenhaAleatoria(int *senhas, bool *prioridades, int *numSenhasGeradas)
+{
+    if (*numSenhasGeradas > 0)
+    {
+        int indice = rand() % *numSenhasGeradas;
+
+        printf("Consumindo senha: %03d (%s)\n", senhas[indice], prioridades[indice] ? "Prioridade" : "Normal");
+
+        for (int i = indice; i < *numSenhasGeradas - 1; i++)
+        {
+            senhas[i] = senhas[i + 1];
+            prioridades[i] = prioridades[i + 1];
         }
-
-        int indice = rand() % totalSenhas;
-
-        atual = *inicio;
-        Senha *anterior = NULL;
-        while (indice > 0) {
-            anterior = atual;
-            atual = atual->prox;
-            indice--;
-        }
-
-        if (anterior != NULL) {
-            anterior->prox = atual->prox;
-        } else {
-            *inicio = atual->prox;
-        }
-
-        printf("Consumindo senha: %s (%s)\n", atual->codigo, atual->isPrioridade ? "Prioridade" : "Normal");
-        free(atual); 
-    } else {
+        (*numSenhasGeradas)--;
+    }
+    else
+    {
         printf("Não há mais senhas para consumir!\n");
     }
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
 
-    Senha *inicio = NULL;
-    Senha *fim = NULL; 
-
+    int senhas[100];
+    bool prioridades[100];
     int numSenhasGeradas = 0;
 
     int opcao;
-    do {
+    do
+    {
         printf("\nMenu:\n");
         printf("1. Gerar 10 senhas\n");
         printf("2. Consumir senha aleatoriamente\n");
@@ -93,54 +66,30 @@ int main() {
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                printf("Gerando 10 senhas...\n");
-                for (int i = 0; i < 10; i++) {
-                    Senha *novaSenha = (Senha *)malloc(sizeof(Senha));
-                    if (novaSenha == NULL) {
-                        printf("Erro ao alocar memoria para a senha!\n");
-                        exit(1);
-                    }
-
-                    gerarSenha(novaSenha->codigo);
-                    novaSenha->isPrioridade = novaSenha->codigo[0] == 'P';
-
-                    novaSenha->prox = NULL;
-
-                    if (inicio == NULL) {
-                        inicio = novaSenha;
-                        fim = novaSenha;
-                    } else {
-                        fim->prox = novaSenha;
-                        fim = novaSenha;
-                    }
-
-                    numSenhasGeradas++;
-                }
-                printf("Senhas geradas com sucesso!\n");
-                break;
-            case 2:
-                consumirSenhaAleatoria(&inicio);
-                break;
-            case 3:
-                mostrarTodasSenhas(inicio);
-                break;
-            case 0:
-                printf("Saindo do programa...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
+        switch (opcao)
+        {
+        case 1:
+            printf("Gerando 10 senhas...\n");
+            for (int i = 0; i < 10; i++)
+            {
+                gerarSenha(&senhas[numSenhasGeradas], &prioridades[numSenhasGeradas]);
+                numSenhasGeradas++;
+            }
+            printf("Senhas geradas com sucesso!\n");
+            break;
+        case 2:
+            consumirSenhaAleatoria(senhas, prioridades, &numSenhasGeradas);
+            break;
+        case 3:
+            mostrarTodasSenhas(senhas, prioridades, numSenhasGeradas);
+            break;
+        case 0:
+            printf("Tchau!\n");
+            break;
+        default:
+            printf("Opcao invalida!\n");
         }
     } while (opcao != 0);
-
-    
-    Senha *atual = inicio;
-    while (atual != NULL) {
-        Senha *prox = atual->prox;
-        free(atual);
-        atual = prox;
-    }
 
     return 0;
 }
