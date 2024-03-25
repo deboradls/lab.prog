a5b892d883eb9be3b67babd785f76d45f491311d
Débora <deb.limad@gmail.com>
2024-03-25 10:51:34 -0300

    Questão 2

--- Atividade 4/q02.c
+++ Atividade 4/q02.c
@@ -1,47 +1,49 @@
-
-
-// armazenar as senhas
-// corrigir o fato de o codigo forçar a gerar p
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define NUMERO_DE_SENHAS 10
-#define PROBABILIDADE_NORMAL 2
-#define PROBABILIDADE_PRIORITARIA 1
 
-void gerarSenhas()
-{
-    int i, contador_normal = 0, contador_prioritaria = 0;
-
-    printf("Senhas geradas:\n");
-    for (i = 0; i < NUMERO_DE_SENHAS; i++)
-    {
-        // número aleatório para decidir se a senha será normal ou prioritária
-        int probabilidade = rand() % (PROBABILIDADE_NORMAL + PROBABILIDADE_PRIORITARIA);
-        if (probabilidade < PROBABILIDADE_NORMAL && contador_normal < NUMERO_DE_SENHAS * 2 / 3)
-        {
-            contador_normal++;
-            printf("Senha Normal: N%d\n", contador_normal);
-        }
-        else if (contador_prioritaria < NUMERO_DE_SENHAS / 3)
-        {
-            contador_prioritaria++;
-            printf("Senha Prioritária: P%d\n", contador_prioritaria);
-        }
-        else
-        {
-            contador_normal++;
-            printf("Senha Normal: N%d\n", contador_normal);
-        }
+void gerarSenhaNormal(char senha[]) {
+    int i;
+    for (i = 0; i < 3; i++) {
+        senha[i] = 'A' + rand() % 26; // Letras maiúsculas aleatórias
     }
+    senha[3] = rand() % 10 + '0'; // Dígito aleatório
+    senha[4] = '\0'; // Adiciona o terminador de string
 }
 
-int main()
-{
-    // gerador de números aleatórios com base no tempo atual
-    srand(time(NULL));
-    gerarSenhas();
-    return 0;
+void gerarSenhaPrioridade(char senha[]) {
+    int i;
+    for (i = 0; i < 2; i++) {
+        senha[i] = 'P'; // Prefixo 'P' para indicar prioridade
+    }
+    for (i = 2; i < 5; i++) {
+        senha[i] = 'A' + rand() % 26; // Letras maiúsculas aleatórias
+    }
+    senha[5] = rand() % 10 + '0'; // Dígito aleatório
+    senha[6] = '\0'; // Adiciona o terminador de string
+}
+
+int main() {
+    srand(time(NULL)); 
+
+    int i, qtdNormais = 0, qtdPrioridade = 0;
+    char senha[7]; // senha de 6 caracteres + 1 para o terminador
+
+    printf("Gerando senhas de guichê:\n");
+    for (i = 0; i < 10; i++) {
+        if (rand() % 3 == 0 && qtdPrioridade < 3) {
+            gerarSenhaPrioridade(senha);
+            qtdPrioridade++;
+        } else {
+            gerarSenhaNormal(senha);
+            qtdNormais++;
+        }
+        printf("Senha %d: %s\n", i + 1, senha);
+    }
+
+    printf("\nQuantidade de senhas normais: %d\n", qtdNormais);
+    printf("Quantidade de senhas de prioridade: %d\n", qtdPrioridade);
+return 0; 
+
 }
